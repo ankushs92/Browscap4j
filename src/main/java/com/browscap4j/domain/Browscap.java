84e7dbd20a8ec0deca69cf78c5c419bee4381efd
Ankush Sharma <Ankush.s@adsizzlergroup.com>
2016-04-03 19:31:49 +0530

    Fixing indentation

--- src/main/java/com/browscap4j/domain/Browscap.java
+++ src/main/java/com/browscap4j/domain/Browscap.java
@@ -46,12 +46,16 @@ public final class Browscap {
 		PreConditions.checkNull(userAgent, "Cannot pass a null UserAgent String ! ");
 		// Java 8 Magic !
 		logger.debug("Attempting to find BrowserCapabilities for User Agent String {}", userAgent);
-		final String namePattern = regexNamePatternsMap.entrySet().parallelStream()
-				.filter(entry -> userAgent.matches(entry.getValue()))
-				.collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue())).keySet().stream()
-				.max((String n1, String n2) -> {
-					return Integer.compare(n1.length(), n2.length());
-				}).get();
+		final String namePattern = regexNamePatternsMap.entrySet()
+					.parallelStream()
+					.filter(entry -> userAgent.matches(entry.getValue()))
+					.collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()))
+					.keySet()
+					.stream()
+					.max((String n1, String n2) -> {
+						return Integer.compare(n1.length(), n2.length());
+					})
+					.get();
 		final BrowserCapabilities browserCapabilities = cache.get(namePattern);
 		if (browserCapabilities == null) {
 			logger.debug("No Browsercapabilities found for user agent string {} ", userAgent);
