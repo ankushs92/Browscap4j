e4edae5d95ecee8b744469c7a2320097f104741a
Ankush Sharma <ankush.s@adsizzlergroup.com>
2016-09-04 15:06:40 +0530

    simulating travis build

--- src/test/groovy/in/ankushs/browscap4j/BaseSpec.groovy
+++ src/test/groovy/in/ankushs/browscap4j/BaseSpec.groovy
@@ -9,15 +9,18 @@ class BaseSpec extends Specification{
     private static final String URL = "http://browscap.org/stream?q=BrowsCapCSV";
     private static final Logger log = LoggerFactory.getLogger(BaseSpec.class)
 
-    private  File downloadBrowscapFile(){
+    private static File downloadBrowscapFile(){
         log.info("Downloading browscap.csv from $URL")
         def fileLocation =  new BaseSpec().getClass().getClassLoader().getResource("browscap.csv").getFile()
-        def file = new File(fileLocation)
 
-        def out = new BufferedOutputStream(new FileOutputStream(file))
-        out << new URL(URL).openStream()
-        out.close()
-        log.info("Downloading finished")
+        def file = new File(fileLocation)
+        if(file.size() > 100000){
+            def out = new BufferedOutputStream(new FileOutputStream(file))
+            out << new URL(URL).openStream()
+            out.close()
+            log.info("Downloading finished")
+
+        }
         file
     }
 }
